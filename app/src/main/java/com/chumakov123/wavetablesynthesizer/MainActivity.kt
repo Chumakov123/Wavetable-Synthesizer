c61bc7bfd5d89c6bbe078375ceffb85a823abd86
Chumakov123 <Chumakov225@gmail.com>
2026-06-10 10:47:07 +0300

    Hide system bars

--- app/src/main/java/com/chumakov123/wavetablesynthesizer/MainActivity.kt
+++ app/src/main/java/com/chumakov123/wavetablesynthesizer/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.chumakov123.wavetablesynthesizer.ui.WavetableSynthesizerApp
 import com.chumakov123.wavetablesynthesizer.ui.theme.WavetableSynthesizerTheme
 
@@ -19,6 +22,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
 
         lifecycle.addObserver(synthesizer)
 
